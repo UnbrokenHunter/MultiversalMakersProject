3951a77a69ea7e6bc2a2a29b8b2b7f1d50d88fc4
Hunter <huntersfred@gmail.com>
2023-02-26 20:04:26 -0500

    Added Polish and Tutorial
    Added particles, sound effects, and screen shake

--- MultiversalMakers/Assets/_Scripts/Objects/Gate.cs
+++ MultiversalMakers/Assets/_Scripts/Objects/Gate.cs
@@ -10,6 +10,11 @@ namespace MultiversalMakers
         [Header("Settings")]
         [SerializeField] private bool isPermamemt = false;
 
+        [SerializeField] private float stayOpenBonus = .2f;
+
+        [SerializeField] private float openCooldown = 1f;
+        private bool isOnCooldown = false;
+        private bool hasBeenOpened = false;
 
         [Header("Events")]
         [SerializeField] private UnityEvent OpenGate;
@@ -17,12 +22,30 @@ namespace MultiversalMakers
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (isOnCooldown) return;
+            if (isPermamemt && hasBeenOpened) return;
+
             OpenGate?.Invoke();
+            StartCoroutine(StartCooldown());
+            hasBeenOpened = true;
         }
         private void OnTriggerExit2D(Collider2D collision)
         {
             if(!isPermamemt)
-                CloseGate?.Invoke();
+                StartCoroutine(StartStayOpenBonus());
+        }
+
+        private IEnumerator StartStayOpenBonus()
+        {
+            yield return new WaitForSeconds(stayOpenBonus);
+            CloseGate?.Invoke();
+        }
+
+        private IEnumerator StartCooldown()
+        {
+            isOnCooldown = true;
+            yield return new WaitForSeconds(openCooldown);
+            isOnCooldown = false;
         }
         
     }
