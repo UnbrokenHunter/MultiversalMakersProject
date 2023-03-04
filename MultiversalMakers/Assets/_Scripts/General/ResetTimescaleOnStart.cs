a77b0c228b582ec14e0ac4f0ff86d2c2987500c9
lpasquini24 <123428916+lpasquini24@users.noreply.github.com>
2023-03-04 16:32:39 +0800

    Added 3 levels

---
+++ MultiversalMakers/Assets/_Scripts/General/ResetTimescaleOnStart.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace MultiversalMakers
+{
+    public class ResetTimescaleOnStart : MonoBehaviour
+    {
+        [SerializeField] private float defaultTimescale = 1.0f;
+
+        private void Start() => Time.timeScale = defaultTimescale;
+    }
+}
