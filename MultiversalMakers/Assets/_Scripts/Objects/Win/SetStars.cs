d9101174da421f8b09e90fde296cb05b7e3aa954
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2023-03-05 18:28:56 -0500

    Improved Stars in UI

--- MultiversalMakers/Assets/_Scripts/Objects/Win/SetStars.cs
+++ MultiversalMakers/Assets/_Scripts/Objects/Win/SetStars.cs
@@ -1,6 +1,7 @@
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MultiversalMakers
@@ -8,24 +9,63 @@ namespace MultiversalMakers
     public class SetStars : MonoBehaviour
     {
         [SerializeField] private Sprite filledStar;
+        [SerializeField] private float animationTime = 0.05f;
 
         private void OnEnable()
         {
-            SetLevelStars();
+            StartCoroutine(SetLevelStars());
         }
 
         [Button]
-        public void SetLevelStars()
+        public IEnumerator SetLevelStars()
         {
+            yield return new WaitForSecondsRealtime(0.5f);
+
             ChangeImage[] _starImages = GetComponentsInChildren<ChangeImage>();
 
             for (int i = 0; i < GameManager.Instance.StarCount; i++)
             {
-                if (_starImages.Length <= i) return;
+                if (_starImages.Length <= i) yield break;
 
-                _starImages[i].ChangeSprite(filledStar);
+                // Switching the first and the middle star
+                ChangeImage star;
+                if (i == 0) star = _starImages[i + 1];
+                else if (i == 1) star = _starImages[i - 1];
+				else star = _starImages[i];
+
+
+				star.ChangeSprite(filledStar);
+                StartCoroutine(FadeInStar(star));
+
+
+                yield return new WaitForSecondsRealtime(0.5f);
             }
         }
+        private IEnumerator FadeInStar(ChangeImage star)
+        {
+			star.Image.color = new Color(
+				star.Image.color.r,
+				star.Image.color.g,
+				star.Image.color.b,
+	            0);
 
-    }
+
+			while (star.Image.color.a < 250)
+            {
+                Color _color = star.Image.color;
+
+			    _color = new Color(
+                    _color.r,
+                    _color.g, 
+                    _color.b, 
+                    Mathf.Lerp(_color.a, 1, 0.1f));
+
+				star.Image.color = _color;
+
+                yield return new WaitForSecondsRealtime(animationTime);
+
+            }
+		}
+
+	}
 }
