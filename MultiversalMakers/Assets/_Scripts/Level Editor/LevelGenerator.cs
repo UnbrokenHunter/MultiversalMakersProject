9bd35b9dc6713484a6eb25889a3d145e8f9b4099
Hunter <huntersfred@gmail.com>
2023-03-15 17:28:16 -0400

    Build

--- MultiversalMakers/Assets/_Scripts/Level Editor/LevelGenerator.cs
+++ MultiversalMakers/Assets/_Scripts/Level Editor/LevelGenerator.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using static MultiversalMakers.LevelGenerator.ObjectData;
-using static UnityEditor.PlayerSettings;
 
 namespace MultiversalMakers
 {
--- MultiversalMakers/Assets/_Scripts/Scene/LevelSelect.cs
+++ MultiversalMakers/Assets/_Scripts/Scene/LevelSelect.cs
@@ -1,6 +1,4 @@
 using ProjectBeelzebub;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MultiversalMakers
